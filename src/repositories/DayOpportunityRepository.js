8e5627e1a7832c36d4596ea4a1a4ac5a65dab89f
Pedro Tashima <pedrotashima42@gmail.com>
2021-05-02 18:17:17 -0300

    chore(database): removed unused methods
    some methods were added for testing purposes and aren't needed anymore

--- src/repositories/DayOpportunityRepository.js
+++ src/repositories/DayOpportunityRepository.js
@@ -1,9 +1,6 @@
 export default function buildDayOpportunityRepository({ DayOpportunityDocument, datesUtils }) {
   return Object.freeze({
     create,
-    findAll,
-    findById,
-    findByProperties,
     findByDay,
     findByDayPopulate,
     updateById,
@@ -11,24 +8,6 @@ export default function buildDayOpportunityRepository({ DayOpportunityDocument, 
   async function create({ ...data }) {
     const created = DayOpportunityDocument.create({ ...data })
     return created
-  }
-
-  async function findAll() {
-    const all = await DayOpportunityDocument.find({})
-    return all
-  }
-
-  async function findById(id) {
-    const rawFound = await DayOpportunityDocument.find({ _id: id })
-    const found = rawFound[0]
-    if (!found._id) return null
-    return found
-  }
-
-  async function findByProperties({ ...properties }) {
-    const found = await DayOpportunityDocument.find({ ...properties })
-    if (!found[0]) return null
-    return found
   }
 
   async function findByDay(day = new Date()) {
--- src/repositories/OpportunityRepository.js
+++ src/repositories/OpportunityRepository.js
@@ -1,25 +1,11 @@
 export default function buildOpportunityRepository({ OpportunityDocument }) {
   return Object.freeze({
     create,
-    findAll,
-    findById,
     findByProperties,
   })
   async function create({ ...info }) {
     const created = OpportunityDocument.create({ ...info })
     return created
-  }
-
-  async function findAll() {
-    const all = await OpportunityDocument.find({})
-    return all
-  }
-
-  async function findById(id) {
-    const rawFound = await OpportunityDocument.find({ _id: id })
-    const found = rawFound[0]
-    if (!found._id) return null
-    return found
   }
 
   async function findByProperties({ ...properties }) {
