3f75d35eb59f3e34acf1af4c14729fa50c8fd839
Pedro Tashima <pedrotashima42@gmail.com>
2021-08-28 18:51:01 -0300

    docs: added api inline documentation

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "clean-architecture-api",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-08-28T21:50:20.640Z",
+    "url": "https://apidocjs.com",
+    "version": "0.29.0"
+  }
+});
--- src/app.js
+++ src/app.js
@@ -8,7 +8,25 @@ import { addOpportunities, listDayOpportunities } from "./controllers/index"
 const app = new Koa()
 const router = Router()
 
+/**
+ * @api {post} /opportunities/integrations
+ * @apiName IntegrateOpportunities
+ * @apiGroup Opportunities
+ * 
+ * @apiSuccess {Boolean} success If true, the operation was a success
+ * @apiSuccess {Array} addedOpportunities Integrated opportunities
+ */
 router.post("/opportunities/integrations", makeCallback(addOpportunities))
+/**
+ * @api {get} /opportunities/day
+ * @apiName DayOpportunities
+ * @apiGroup Opportunities
+ * 
+ * @apiParam {Date} date Day to retrieve integrated opportunities
+ * 
+ * @apiSuccess {Boolean} success If true, the operation was a success
+ * @apiSuccess {Array} dayOpportunities Opportunities integrated on the query day
+ */
 router.get("/opportunities/day", makeCallback(listDayOpportunities))
 
 app
@@ -17,7 +35,7 @@ app
   .use(router.routes())
   .use(router.allowedMethods())
   .use(async ctx => {
-    ctx.body = `<a href="https://github.com/Tashima42/linkapi-teste">🦙</a>`
+    ctx.body = `<a href="https://github.com/Tashima42/clean-architecture-api">🦙</a>`
   })
 
 export default app
